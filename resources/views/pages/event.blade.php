6746f45b814d11813f4467c39cef8abe4d523df4
CJ Thompson <cjthomp@gmail.com>
2018-03-26 01:30:56 -0500

    Pass up page title Fixed sizing of header text Adding event details

--- resources/views/pages/event.blade.php
+++ resources/views/pages/event.blade.php
@@ -1,9 +1,57 @@
 @extends('layouts.framed')
 
+@section('title', 'Event')
+
 @section('content')
+<div class="banner" style="padding: 10px 0 2px;
+    background-color: #56AA46;
+    color: #fff;
+    min-height: 60px;
+    line-height: 35px;"
+>
     <div class="container">
         <div class="row">
-            Event
+            <div class="col-md-10">
+                <h2>
+                    Event / Event Title
+                </h2>
+            </div>
+            <div class="col-md-2">
+                42 Days Ago
+            </div>
         </div>
     </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            31 January to 03 February
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-3">
+            <div class="event-detail-field"></div>
+            <i class="fa fa-user"></i> Created by @username
+            <div class="event-detail-field"></div>
+            <i class="fa fa-user"></i> Technical Director is @username
+            <div class="event-detail-field"></div>
+            <i class="fa fa-user"></i> Production Director is @username
+        </div>
+        <div class="col-md-3">
+            <div class="event-detail-field"></div>
+            <i class="fa fa-user"></i> Venue is Moscone Center
+            <div class="event-detail-field"></div>
+            <i class="fa fa-user"></i> Client is Amazon
+            <div class="event-detail-field"></div>
+            <i class="fa fa-user"></i> Number of Attendees is 2000
+        </div>
+        <div class="offset-md-4">
+            &nbsp;
+        </div>
+        <div class="col-md-2">
+            <img src="" alt="" style="width: 128px; height: 128px;">
+        </div>
+    </div>
+</div>
 @endsection
